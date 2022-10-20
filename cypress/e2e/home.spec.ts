67090c2281be595377455588fd6581d8c7f0af2a
Isma Navarro <ismanapa@gmail.com>
2022-10-20 16:56:39 +0200

    feat: add config file and blank dashboard component
    Co-authored-by: Javier Ferrer González <javier.ferrer@codely.com>

--- cypress/e2e/home.spec.ts
+++
@@ -1,8 +0,0 @@
-describe("The Home Page", () => {
-	it("successfully loads", () => {
-		cy.visit("/");
-		cy.findByRole("heading", { name: /🌱⚛️ Create React App Codely template example/i }).should(
-			"exist"
-		);
-	});
-});
