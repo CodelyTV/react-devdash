c9bccf06c0589baa1e16b9b2d5fa2b13ce53b86d
Isma Navarro <ismanapa@gmail.com>
2022-10-25 16:24:30 +0200

    feat: render widget list and some semantic web refactoring
    Co-authored-by: Javier Ferrer González <javier.ferrer@codely.com>

--- src/sections/dashboard/Dashboard.tsx
+++ src/sections/dashboard/Dashboard.tsx
@@ -1,7 +1,22 @@
+import React from "react";
+
+import { config } from "../../devdash_config";
+
 export function Dashboard() {
+	const title = "DevDash_";
+
 	return (
-		<div>
-			<h1>DevDash_</h1>
-		</div>
+		<>
+			<head>
+				<h1>{title}</h1>
+			</head>
+			<section>
+				<ul>
+					{config.widgets.map((widget) => (
+						<li key={widget.id}>{widget.repository_url}</li>
+					))}
+				</ul>
+			</section>
+		</>
 	);
 }
