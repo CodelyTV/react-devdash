b8592e4007994401c9ca3ca7a5a0f77a910038c2
Isma Navarro <ismanapa@gmail.com>
2022-11-10 18:06:03 +0100

    feat: add detail page

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import { DashboardFactory } from "./sections/dashboard/DashboardFactory";
+import { GitHubRepositoryDetail } from "./sections/gitHubRepositoryDetail/GitHubRepositoryDetail";
 
 const router = createBrowserRouter([
 	{
 		path: "/",
 		element: DashboardFactory.create(),
+	},
+	{
+		path: "/repository/:organization/:name",
+		element: <GitHubRepositoryDetail />,
 	},
 ]);
 
