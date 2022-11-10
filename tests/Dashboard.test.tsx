f41a09167d6359f5f0e7480e79bea97046ee195f
Isma Navarro <ismanapa@gmail.com>
2022-11-10 18:19:21 +0100

    feat: add navigation to detail page from dashboard

--- tests/Dashboard.test.tsx
+++ tests/Dashboard.test.tsx
@@ -1,9 +1,10 @@
-import { render, screen } from "@testing-library/react";
+import { screen } from "@testing-library/react";
 import { mock } from "jest-mock-extended";
 
 import { GitHubRepositoryRepository } from "../src/domain/GitHubRepositoryRepository";
 import { Dashboard } from "../src/sections/dashboard/Dashboard";
 import { GitHubRepositoryMother } from "./GitHubRepositoryMother";
+import { renderWithRouter } from "./renderWithRouter";
 
 const mockRepository = mock<GitHubRepositoryRepository>();
 
@@ -13,7 +14,7 @@ describe("Dashboard section", () => {
 
 		mockRepository.search.mockResolvedValue([gitHubRepository]);
 
-		render(<Dashboard repository={mockRepository} />);
+		renderWithRouter(<Dashboard repository={mockRepository} />);
 
 		const firstWidgetTitle = `${gitHubRepository.id.organization}/${gitHubRepository.id.name}`;
 		const firstWidgetHeader = await screen.findByRole("heading", {
@@ -26,7 +27,7 @@ describe("Dashboard section", () => {
 	it("show not results message when there are no widgets", async () => {
 		mockRepository.search.mockResolvedValue([]);
 
-		render(<Dashboard repository={mockRepository} />);
+		renderWithRouter(<Dashboard repository={mockRepository} />);
 
 		const noResults = await screen.findByText(new RegExp("No hay widgets configurados", "i"));
 
@@ -38,7 +39,7 @@ describe("Dashboard section", () => {
 
 		mockRepository.search.mockResolvedValue([gitHubRepository]);
 
-		render(<Dashboard repository={mockRepository} />);
+		renderWithRouter(<Dashboard repository={mockRepository} />);
 
 		const modificationDate = await screen.findByText(new RegExp("today", "i"));
 
