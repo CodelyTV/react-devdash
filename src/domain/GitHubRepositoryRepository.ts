7ffa9b6b77949b5c63d1187d1dd5514514ee0304
Isma Navarro <ismanapa@gmail.com>
2022-11-22 23:57:59 +0100

    feat: add GitHubRepositoryDetail factory and custom hook

--- src/domain/GitHubRepositoryRepository.ts
+++ src/domain/GitHubRepositoryRepository.ts
@@ -1,5 +1,6 @@
-import { GitHubRepository } from "./GitHubRepository";
+import { GitHubRepository, RepositoryId } from "./GitHubRepository";
 
 export interface GitHubRepositoryRepository {
 	search(repositoryUrls: string[]): Promise<GitHubRepository[]>;
+	byId(repositoryId: RepositoryId): Promise<GitHubRepository | undefined>;
 }
