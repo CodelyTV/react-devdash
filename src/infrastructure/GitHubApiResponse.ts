222fa460900953aca9b2705907748335e5c69917
Javier Ferrer <javier.mailserio@gmail.com>
2022-11-30 18:04:46 +0100

    style: disable eslint errors on the automatically generated GitHub Api Responses because we are not going to use these properties

--- src/infrastructure/GitHubApiResponse.ts
+++ src/infrastructure/GitHubApiResponse.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 interface Permissions {
 	admin: boolean;
 	maintain: boolean;
--- src/sections/layout/TopBarProgressByLocation.tsx
+++ src/sections/layout/TopBarProgressByLocation.tsx
@@ -2,6 +2,14 @@ import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import TopBarProgress from "react-topbar-progress-indicator";
 
+TopBarProgress.config({
+	barColors: {
+		"0": "#fff",
+		"1.0": "#3cff64",
+	},
+	shadowBlur: 5,
+});
+
 const TopBarProgressByLocation = () => {
 	const [progress, setProgress] = useState(false);
 	const [previousLocation, setPreviousLocation] = useState("");
