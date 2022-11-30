1f07bc4b7ce7a704a9c40aac93d3e35ad8df9f23
Javier Ferrer <javier.mailserio@gmail.com>
2022-11-30 17:31:18 +0100

    feat: basic route loading bar based on `location.path`

--- src/sections/layout/Layout.tsx
+++ src/sections/layout/Layout.tsx
@@ -1,12 +1,23 @@
 import { Link, Outlet } from "react-router-dom";
+import TopBarProgress from "react-topbar-progress-indicator";
 
 import { ReactComponent as Brand } from "./brand.svg";
 import { ErrorBoundary } from "./ErrorBoundary";
 import styles from "./Layout.module.scss";
+import TopBarProgressByLocation from "./TopBarProgressByLocation";
+
+TopBarProgress.config({
+	barColors: {
+		"0": "#fff",
+		"1.0": "#3cff64",
+	},
+	shadowBlur: 5,
+});
 
 export function Layout() {
 	return (
 		<>
+			<TopBarProgressByLocation />
 			<header className={styles.header}>
 				<section className={styles.header__container}>
 					<a href="https://codely.com">
