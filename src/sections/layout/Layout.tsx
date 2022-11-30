5d5e407ce5e70c73483180432dfc1aef76cf5e6a
Javier Ferrer <javier.mailserio@gmail.com>
2022-11-30 16:59:33 +0100

    feat: link from header items to the home in order to make it easier to navigate from widget details page

--- src/sections/layout/Layout.tsx
+++ src/sections/layout/Layout.tsx
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 import { ReactComponent as Brand } from "./brand.svg";
 import { ErrorBoundary } from "./ErrorBoundary";
@@ -9,8 +9,12 @@ export function Layout() {
 		<>
 			<header className={styles.header}>
 				<section className={styles.header__container}>
-					<Brand />
-					<h1 className={styles.app__brand}>DevDash_</h1>
+					<a href="https://codely.com">
+						<Brand />
+					</a>
+					<Link to={`/`}>
+						<h1 className={styles.app__brand}>DevDash_</h1>
+					</Link>
 				</section>
 			</header>
 			<ErrorBoundary>
