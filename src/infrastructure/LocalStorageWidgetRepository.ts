8a890ff4bcdcd6d3dbc2076ed4ee66a727611fe4
Isma Navarro <ismanapa@gmail.com>
2022-12-12 12:31:30 +0100

    test: [red] show error when respository already exists in Dashboard
    Co-authored-by: Javier Ferrer González <javier.ferrer@codely.com>

--- src/infrastructure/LocalStorageWidgetRepository.ts
+++ src/infrastructure/LocalStorageWidgetRepository.ts
@@ -2,6 +2,10 @@ import { RepositoryWidget } from "../domain/RepositoryWidget";
 import { RepositoryWidgetRepository } from "../domain/RepositoryWidgetRepository";
 
 export class LocalStorageRepositoryWidgetRepository implements RepositoryWidgetRepository {
+	async search(): Promise<RepositoryWidget[]> {
+		return Promise.resolve([]);
+	}
+
 	async save(widget: RepositoryWidget): Promise<void> {
 		await Promise.resolve();
 	}
